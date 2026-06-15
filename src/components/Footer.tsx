68c4ef6bdbcb23c3fdffe4f071690894fae835cb
Alexandru <alexbran8@gmail.com>
2026-06-15 12:29:22 +0200

    fix: footer on desktop

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -18,7 +18,8 @@ export const Footer = () => {
         paddingY="8"
         paddingX="16"
         gap="16"
-        horizontal="between"
+        direction="column"
+        horizontal="center"
         vertical="center"
         s={{
           direction: "column",
@@ -26,16 +27,24 @@ export const Footer = () => {
           vertical: "center",
         }}
       >
-        <Text variant="body-default-s" onBackground="neutral-strong">
-          <Text onBackground="neutral-weak">© {currentYear} /</Text>
-          <Text paddingX="4">{person.name}</Text>
+        <Text className={styles.textStack} variant="body-default-s" onBackground="neutral-strong">
+          <Text onBackground="neutral-weak">© {currentYear}</Text>
+          <Text>{person.name}</Text>
           <Text onBackground="neutral-weak">
             {/* Usage of this template requires attribution. Please don't remove the link to Once UI unless you have a Pro license. */}
-            / {localized.footer.attribution}{" "}
+            {localized.footer.attribution}{" "}
             <SmartLink href="https://once-ui.com/products/magic-portfolio">Once UI</SmartLink>
           </Text>
         </Text>
-        <Row gap="16">
+        <Row
+          className={styles.iconStack}
+          gap="16"
+          s={{
+            direction: "column",
+            horizontal: "center",
+            vertical: "center",
+          }}
+        >
           {social.map(
             (item) =>
               item.link && (
